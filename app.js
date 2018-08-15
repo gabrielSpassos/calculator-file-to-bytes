67ce6447559b4765af93114fb25ce4a1996fd766
Gabriel Passos <gabriel.passos@ilegra.com>
2018-08-15 16:09:34 -0300

    :recycle: reajuste na maneira de calcular os bytes

--- app.js
+++ app.js
@@ -3,5 +3,15 @@ const fs = require('fs');
 const test = fs.readFileSync('test.txt', 'utf8');
 console.log(test);
 
-const testValue = encodeURI(test).split(/%..|./).length - 1;
-console.log(testValue);
+console.log('Bytes number = ' + byteLength(test));
+
+function byteLength(str) {
+    let s = str.length;
+    for (let i=str.length-1; i>=0; i--) {
+        var code = str.charCodeAt(i);
+        if (code > 0x7f && code <= 0x7ff) s++;
+        else if (code > 0x7ff && code <= 0xffff) s+=2;
+        if (code >= 0xDC00 && code <= 0xDFFF) i--; //trail surrogate
+    }
+    return s;
+}
